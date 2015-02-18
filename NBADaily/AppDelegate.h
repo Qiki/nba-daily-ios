9e06445716ac5e1358d747de6ed5c6bff9c6f1cf
Qiki <qi@brandingbrand.com>
2015-02-18 17:41:15 -0500

    Initial Commit

---
+++ NBADaily/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NBADaily
+//
+//  Created by kiki on 2/18/15.
+//  Copyright (c) 2015 kiki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NBADaily/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NBADaily
+//
+//  Created by kiki on 2/18/15.
+//  Copyright (c) 2015 kiki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
