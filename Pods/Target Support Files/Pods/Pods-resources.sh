5190fe2e44e91764e1befbb8620faa183a22a27f
Qiki <qi@brandingbrand.com>
2015-12-22 14:09:03 -0500

    added chrome cast pod

--- Pods/Target Support Files/Pods/Pods-resources.sh
+++ Pods/Target Support Files/Pods/Pods-resources.sh
@@ -57,6 +57,14 @@ install_resource()
       ;;
   esac
 }
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_resource "google-cast-sdk/GoogleCastSDK-2.10.1-Release/GoogleCast.framework/GoogleCastResources.bundle"
+  install_resource "${BUILT_PRODUCTS_DIR}/CastFrameworkAssets.bundle"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_resource "google-cast-sdk/GoogleCastSDK-2.10.1-Release/GoogleCast.framework/GoogleCastResources.bundle"
+  install_resource "${BUILT_PRODUCTS_DIR}/CastFrameworkAssets.bundle"
+fi
 
 mkdir -p "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
